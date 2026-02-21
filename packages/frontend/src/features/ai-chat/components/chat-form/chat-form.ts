70fd35644251e37d37e6f0336e1e19442e7d9de1
mikhalenkadaniil <mikhalenka.d@gmail.com>
2026-02-21 16:22:22 +0100

    feat: implement responsive chat-form component

---
+++ packages/frontend/src/features/ai-chat/components/chat-form/chat-form.ts
@@ -0,0 +1,33 @@
+import { Component, output } from '@angular/core';
+import { ReactiveFormsModule , FormGroup, FormControl } from '@angular/forms';
+
+const padding = 12;
+const lineHeight = 16;
+const baseTextAreaHeight = padding * 2 + lineHeight;
+
+@Component({
+  selector: 'app-chat-form',
+  imports: [ReactiveFormsModule],
+  templateUrl: './chat-form.html',
+  styleUrl: './chat-form.scss',
+})
+export class ChatFormComponent {
+  readonly sendMessageEvent = output<string>();
+
+  messageForm = new FormGroup({
+    message: new FormControl(''),
+  });
+
+  autoResize(event: Event) {
+    const textarea = event.target as HTMLTextAreaElement;
+    textarea.style.height = 'auto';
+    textarea.style.height = `${(textarea.scrollHeight - baseTextAreaHeight).toString()} + px`;
+  }
+
+  handleSubmit() {
+    const message = this.messageForm.value.message;
+    if (message) {
+      this.sendMessageEvent.emit(message);
+    }
+  }
+}
