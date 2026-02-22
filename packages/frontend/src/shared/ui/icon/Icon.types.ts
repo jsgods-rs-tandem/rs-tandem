84b16ca7dd442e787f14477f7ea75f716e9d6db6
Boris Zashliapin <test-zashliapin-b@yandex.ru>
2026-02-22 14:19:20 +0300

    feat: create ui component icon

---
+++ packages/frontend/src/shared/ui/icon/Icon.types.ts
@@ -0,0 +1,5 @@
+type IconName = 'ai' | 'arrow-up' | 'code' | 'cube' | 'earth' | 'github' | 'minus' | 'plus' | 'sun';
+type IconSize = 'm' | 'l';
+type IconColor = 'neutral' | 'brand';
+
+export type { IconName, IconSize, IconColor };
---
+++ packages/frontend/src/shared/ui/icon/icon.component.ts
@@ -0,0 +1,21 @@
+import { Component, input } from '@angular/core';
+
+import type { IconName, IconSize, IconColor } from './Icon.types';
+
+@Component({
+  selector: 'app-icon',
+  templateUrl: './icon.component.html',
+  styleUrl: './icon.component.scss',
+  standalone: true,
+})
+export class IconComponent {
+  name = input.required<IconName>();
+  size = input<IconSize | undefined>('m');
+  color = input<IconColor | undefined>('neutral');
+
+  private readonly _spritePath = '@/assets/icons/sprite.svg';
+
+  get href(): string {
+    return `${this._spritePath}#${this.name()}`;
+  }
+}
