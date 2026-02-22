3f6c7600145143d62b5f6d705dd2fa2dddfcef44
Daniil <mikhalenka.d@gmail.com>
2026-02-22 23:40:44 +0100

    feat: create Chat Form (#32)
    * feat: implement responsive chat-form component
    
    * fix: correct height of text area to a valid value in autoResize
    
    * feat: reset form after submiting
    
    * feat: add aria label for submit button
    
    * fix: protect the form submission from empty value
    
    * fix: move aria-label from text area to submit button
    
    * chore: rename files to adding component suffix
    
    * fix: change pathes to correct
    
    * fix: protect autoResize method from unexpected type
    
    * feat: implement submiting form by pressing Enter
    
    * chore: rename spec file
    
    * feat: remove unnecessary styles
    
    * feat: add outline
    
    * refactor: move type check logic into guard
    
    * fix: update package lock json to avoid difference with dev branch
    
    * chore: add Component type to angular.json (#25)
    
    * chore: add global basic abstract styles
    
    * feat: create base angular template
    
    * fix: eslint no unresolved import rule
    
    * fix: set minimal height for text-area
    
    * fix: resize text-area after submit
    
    * chore: delete unnecessary guard
    
    ---------
    
    Co-authored-by: Diana <127693483+dukhd@users.noreply.github.com>
    Co-authored-by: Boris Zashliapin <test-zashliapin-b@yandex.ru>

---
+++ packages/frontend/src/features/ai-chat/components/chat-form/chat-form.component.ts
@@ -0,0 +1,42 @@
+import { Component, output } from '@angular/core';
+import { ReactiveFormsModule, FormGroup, FormControl } from '@angular/forms';
+
+const padding = 12;
+const lineHeight = 16;
+const baseTextAreaHeight = padding * 2 + lineHeight;
+
+@Component({
+  selector: 'app-chat-form',
+  imports: [ReactiveFormsModule],
+  templateUrl: './chat-form.component.html',
+  styleUrl: './chat-form.component.scss',
+})
+export class ChatFormComponent {
+  readonly sendMessageEvent = output<string>();
+
+  messageForm = new FormGroup({
+    message: new FormControl(''),
+  });
+
+  autoResize(textarea: HTMLTextAreaElement) {
+    textarea.style.height = 'auto';
+    textarea.style.height = `${(textarea.scrollHeight - baseTextAreaHeight).toString()}px`;
+  }
+
+  submitOnEnter(event: Event, textarea: HTMLTextAreaElement) {
+    if (event instanceof KeyboardEvent) {
+      if (event.shiftKey) return;
+      event.preventDefault();
+      this.handleSubmit(textarea);
+    }
+  }
+
+  handleSubmit(textarea: HTMLTextAreaElement) {
+    const message = this.messageForm.value.message;
+    if (message?.trim()) {
+      this.sendMessageEvent.emit(message);
+      this.messageForm.reset();
+      this.autoResize(textarea);
+    }
+  }
+}
