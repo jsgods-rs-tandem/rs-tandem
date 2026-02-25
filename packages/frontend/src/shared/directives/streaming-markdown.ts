939a74462ff587afc3d3cd134807da8e2928cc2c
mikhalenkadaniil <mikhalenka.d@gmail.com>
2026-02-25 21:10:52 +0100

    refactor: isolate cycle of chunks in streaming md directive

--- packages/frontend/src/shared/directives/streaming-markdown.ts
+++ packages/frontend/src/shared/directives/streaming-markdown.ts
@@ -1,17 +1,35 @@
-import { Directive, ElementRef, OnDestroy, inject } from '@angular/core';
+import { Directive, ElementRef, OnDestroy, inject, input, OnInit } from '@angular/core';
 import DOMPurify from 'dompurify';
+import { ChatResponse } from 'ollama';
 import * as smd from 'streaming-markdown';
 
 @Directive({
   selector: '[appStreamingMarkdown]',
-  exportAs: 'streamMd',
+  standalone: true,
 })
-export class StreamingMarkdownDirective implements OnDestroy {
+export class StreamingMarkdownDirective implements OnDestroy, OnInit {
+  readonly stream = input.required<AsyncIterable<ChatResponse>>();
   private readonly el = inject<ElementRef<HTMLElement>>(ElementRef);
   private readonly renderer = smd.default_renderer(this.el.nativeElement);
   private readonly parser = smd.parser(this.renderer);
 
-  write(chunk: string) {
+  ngOnInit() {
+    void this.writeStream();
+  }
+
+  ngOnDestroy() {
+    this.end();
+  }
+
+  private async writeStream() {
+    const mdStream = this.stream();
+
+    for await (const part of mdStream) {
+      this.write(part.message.content);
+    }
+  }
+
+  private write(chunk: string) {
     const sanitized = DOMPurify.sanitize(chunk);
 
     if (DOMPurify.removed.length) {
@@ -22,11 +40,7 @@ export class StreamingMarkdownDirective implements OnDestroy {
     smd.parser_write(this.parser, sanitized);
   }
 
-  end() {
+  private end() {
     smd.parser_end(this.parser);
-  }
-
-  ngOnDestroy() {
-    this.end();
   }
 }
