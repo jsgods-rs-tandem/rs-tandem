f1672c4c5ca6e1dc5bcd04bc731fad08e7c43f87
Diana <127693483+dukhd@users.noreply.github.com>
2026-02-26 16:44:20 +0000

    feat: create button component (#51)
    * feat: create button component
    
    * refactor: address review feedback for ButtonComponent

---
+++ packages/frontend/src/shared/ui/button/button.component.ts
@@ -0,0 +1,31 @@
+import { Component, computed, input } from '@angular/core';
+import type { ButtonType } from '@/shared/types';
+import { IconComponent } from '../icon/icon.component';
+import { RouterModule } from '@angular/router';
+import { ButtonVersion } from './button.types';
+import { IconColor, IconName } from '../icon/Icon.types';
+import { NgTemplateOutlet } from '@angular/common';
+
+@Component({
+  selector: 'app-button',
+  imports: [NgTemplateOutlet, IconComponent, RouterModule],
+  templateUrl: './button.component.html',
+  styleUrl: './button.component.scss',
+})
+export class ButtonComponent {
+  type = input<ButtonType>('button');
+  version = input<ButtonVersion>('primary');
+  text = input<string>('');
+  icon = input<IconName>();
+  iconColor = input<IconColor>('secondary');
+  /** for Angular Router */
+  link = input<string>();
+  /** for links like GitHub or #about */
+  href = input<string>();
+  /** Open link in a new tab if true.*/
+  external = input<boolean>(false);
+  disabled = input<boolean>(false);
+  ariaLabel = input<string>('');
+  displayAriaLabel = computed(() => this.ariaLabel() || this.text());
+  classes = computed(() => `button button_${this.version()}`);
+}
--- packages/frontend/src/shared/ui/icon/Icon.types.ts
+++ packages/frontend/src/shared/ui/icon/Icon.types.ts
@@ -1,5 +1,5 @@
 type IconName = 'ai' | 'arrow-up' | 'code' | 'cube' | 'earth' | 'github' | 'minus' | 'plus' | 'sun';
 type IconSize = 'm' | 'l';
-type IconColor = 'neutral' | 'brand';
+type IconColor = 'neutral' | 'brand' | 'secondary';
 
 export type { IconName, IconSize, IconColor };
