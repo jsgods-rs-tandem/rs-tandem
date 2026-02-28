4b356c8ee412b2e1ac3f513decdeb70dca9a4d89
mikhalenkadaniil <mikhalenka.d@gmail.com>
2026-02-28 17:29:27 +0100

    feat: improve streaming-markdown directive by render static markdown

--- packages/frontend/src/shared/directives/streaming-markdown.directive.ts
+++ packages/frontend/src/shared/directives/streaming-markdown.directive.ts
@@ -7,23 +7,29 @@ import * as smd from 'streaming-markdown';
   selector: '[appStreamingMarkdown]',
 })
 export class StreamingMarkdownDirective implements OnDestroy, OnInit {
-  readonly stream = input.required<AsyncIterable<ChatResponse>>({ alias: 'appStreamingMarkdown' });
+  readonly markdown = input.required<AsyncIterable<ChatResponse> | string>({
+    alias: 'appStreamingMarkdown',
+  });
   private readonly el = inject<ElementRef<HTMLElement>>(ElementRef);
   private readonly renderer = smd.default_renderer(this.el.nativeElement);
   private readonly parser = smd.parser(this.renderer);
   readonly streamingMarkdownErrorEvent = output<unknown>();
 
   ngOnInit() {
-    void this.writeStream();
+    const markdown = this.markdown();
+
+    if (typeof markdown === 'string') {
+      this.write(markdown);
+    } else {
+      void this.writeStream(markdown);
+    }
   }
 
   ngOnDestroy() {
     this.end();
   }
 
-  private async writeStream() {
-    const mdStream = this.stream();
-
+  private async writeStream(mdStream: AsyncIterable<ChatResponse>) {
     try {
       for await (const part of mdStream) {
         this.write(part.message.content);
