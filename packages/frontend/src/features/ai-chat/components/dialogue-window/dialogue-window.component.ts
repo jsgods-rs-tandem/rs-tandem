2b79c6e9d5ae7f7fe079adccb88588e970a11722
mikhalenkadaniil <mikhalenka.d@gmail.com>
2026-02-28 18:08:53 +0100

    feat: render assistant messages

--- packages/frontend/src/features/ai-chat/components/dialogue-window/dialogue-window.component.ts
+++ packages/frontend/src/features/ai-chat/components/dialogue-window/dialogue-window.component.ts
@@ -1,12 +1,13 @@
 import { Component, input } from '@angular/core';
-import { IMessage } from '../../models/llm-message-model';
+import { IMessage, IStreamMessage } from '../../models/llm-message-model';
+import { MarkdownDirective } from '@/shared/directives/markdown.directive';
 
 @Component({
   selector: 'app-dialogue-window',
-  imports: [],
+  imports: [MarkdownDirective],
   templateUrl: './dialogue-window.component.html',
   styleUrl: './dialogue-window.component.scss',
 })
 export class DialogueWindowComponent {
-  history = input.required<IMessage[]>();
+  messsages = input.required<(IMessage | IStreamMessage)[]>();
 }
