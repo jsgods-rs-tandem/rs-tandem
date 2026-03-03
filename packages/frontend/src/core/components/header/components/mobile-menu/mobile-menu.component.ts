3d3a0c0aed5f4041e1bd462df679c439dc2c100f
Diana Dukhovskaya <k.dianavv@gmail.com>
2026-03-03 21:20:51 +0000

    feat: implement scroll lock when mobile menu open

--- packages/frontend/src/core/components/header/components/mobile-menu/mobile-menu.component.ts
+++ packages/frontend/src/core/components/header/components/mobile-menu/mobile-menu.component.ts
@@ -1,5 +1,15 @@
 import { ButtonComponent } from '@/shared/ui';
-import { ChangeDetectionStrategy, Component, HostListener, input, output } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  DOCUMENT,
+  effect,
+  HostListener,
+  inject,
+  input,
+  output,
+  Renderer2,
+} from '@angular/core';
 
 @Component({
   selector: 'app-mobile-menu',
@@ -9,9 +19,20 @@ import { ChangeDetectionStrategy, Component, HostListener, input, output } from 
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class MobileMenuComponent {
+  private renderer = inject(Renderer2);
+  private document = inject(DOCUMENT);
   isOpen = input<boolean>(false);
   menuToggled = output();
-
+  constructor() {
+    effect((onCleanup) => {
+      if (this.isOpen()) {
+        this.renderer.addClass(this.document.body, 'no-scroll');
+        onCleanup(() => {
+          this.renderer.removeClass(this.document.body, 'no-scroll');
+        });
+      }
+    });
+  }
   @HostListener('window:resize')
   onResize(): void {
     if (this.isOpen() && window.innerWidth > 768) {
