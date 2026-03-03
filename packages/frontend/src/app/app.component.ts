382d04b725bf0d7af83ddc352bcb5de11f1bade5
Diana Dukhovskaya <k.dianavv@gmail.com>
2026-03-03 20:47:55 +0000

    feat: implement routing logic

--- packages/frontend/src/app/app.component.ts
+++ packages/frontend/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { FooterComponent } from '@/core/components/footer/footer.component';
 import { HeaderComponent } from '@/core/components/header/header.component';
 import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent, FooterComponent],
+  imports: [HeaderComponent, FooterComponent, RouterOutlet],
   standalone: true,
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
--- packages/frontend/src/main.ts
+++ packages/frontend/src/main.ts
@@ -1,9 +1,7 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
-import { provideRouter } from '@angular/router';
+import { appConfig } from './app/app.config';
 
-bootstrapApplication(AppComponent, {
-  providers: [provideRouter([])],
-}).catch((error: unknown) => {
+bootstrapApplication(AppComponent, appConfig).catch((error: unknown) => {
   console.error(error);
 });
