7b285df071f5109ee5c54b4056c304a36ee4896b
Diana <127693483+dukhd@users.noreply.github.com>
2026-03-04 21:15:58 +0000

    feat: implement footer component (#74)
    * feat: implement footer component
    
    * refactor: apply code review suggestions

---
+++ packages/frontend/src/core/components/footer/footer.component.ts
@@ -0,0 +1,13 @@
+import { EXTERNAL_LINKS } from '@/core/constants/external-links';
+import { ButtonComponent } from '@/shared/ui';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-footer',
+  imports: [ButtonComponent],
+  templateUrl: './footer.component.html',
+  styleUrl: './footer.component.scss',
+})
+export class FooterComponent {
+  readonly githubUrl = EXTERNAL_LINKS.githubRepo;
+}
