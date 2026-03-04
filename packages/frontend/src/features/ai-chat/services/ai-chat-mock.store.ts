b9d2fecdf4c9ba7bcc276fc055f62b8d92508629
mikhalenkadaniil <mikhalenka.d@gmail.com>
2026-03-04 18:14:10 +0100

    feat: observe chat status

--- packages/frontend/src/features/ai-chat/services/ai-chat-mock.store.ts
+++ packages/frontend/src/features/ai-chat/services/ai-chat-mock.store.ts
@@ -2,6 +2,7 @@ import { Injectable, signal, computed } from '@angular/core';
 import { IMessage, IStreamMessage } from '../models/llm-message-model';
 import { OllamaService } from './ollama-service';
 import { isAsyncIterable } from '@/core/guards/is-async-iterable';
+import { ChatStatus } from '../models/ai-chat-status';
 
 @Injectable({
   providedIn: 'root',
@@ -9,23 +10,34 @@ import { isAsyncIterable } from '@/core/guards/is-async-iterable';
 export class AiChatMockStore {
   private llm = new OllamaService();
   private _messages = signal<(IMessage | IStreamMessage)[]>([]);
+  private _status = signal<ChatStatus>('default');
 
   readonly messages = this._messages.asReadonly();
   readonly messagesLength = computed(() => this._messages().length);
+  readonly status = this._status.asReadonly();
 
   sendPrompt(text: string) {
-    const message: IMessage = { role: 'user', content: text };
-    this._messages.update((array) => [...array, message]);
-    void this.send(message);
+    if (this.status() === 'default') {
+      const message: IMessage = { role: 'user', content: text };
+      this._messages.update((array) => [...array, message]);
+      void this.send(message);
+    }
+  }
+
+  updateStatus(status: ChatStatus) {
+    this._status.set(status);
   }
 
   private async send(message: IMessage) {
+    this.updateStatus('pending');
     const response = await this.llm.sendPrompt([message], 'gemma3:1b');
 
     if (isAsyncIterable(response)) {
       const message: IStreamMessage = { role: 'assistant', content: response };
 
       this._messages.update((array) => [...array, message]);
+    } else {
+      this.updateStatus('error');
     }
   }
 }
