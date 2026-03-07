aff91d88e99cb1d955e2cb122549f0afb1ba56ed
Boris Zashliapin <test-zashliapin-b@yandex.ru>
2026-03-07 13:40:10 +0300

    feat: create ui component - Badge

---
+++ packages/frontend/src/features/quiz/ui/badge/badge.utilities.ts
@@ -0,0 +1,17 @@
+import { REWARD_LEVEL, REWARD_SCORE } from './badge.constants';
+
+export const computeRewardLevel = (score: number) => {
+  if (score >= REWARD_SCORE[REWARD_LEVEL.senior]) {
+    return REWARD_LEVEL.senior;
+  }
+
+  if (score >= REWARD_SCORE[REWARD_LEVEL.middle]) {
+    return REWARD_LEVEL.middle;
+  }
+
+  if (score >= REWARD_SCORE[REWARD_LEVEL.junior]) {
+    return REWARD_LEVEL.junior;
+  }
+
+  return REWARD_LEVEL.trainee;
+};
