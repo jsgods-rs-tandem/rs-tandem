ccb11480841a40f0a0d12b8b234f0cab7a15a8dd
Diana Dukhovskaya <k.dianavv@gmail.com>
2026-03-09 22:08:54 +0000

    refactor: simplify team-card component

--- packages/frontend/src/shared/ui/team-member-card/team-member-card.component.ts
+++ packages/frontend/src/shared/ui/team-member-card/team-member-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, computed, input } from '@angular/core';
+import { Component, input } from '@angular/core';
 import { ButtonComponent } from '../button/button.component';
 import { IconName } from '../icon/Icon.types';
 
@@ -11,18 +11,9 @@ import { IconName } from '../icon/Icon.types';
 export class TeamMemberCardComponent {
   name = input.required<string>();
   avatarUrl = input.required<string>();
-
-  githubLogin = input<string>();
-  externalLink = input<string>();
-  linkText = input<string>();
-
-  displayHref = computed(() => {
-    const external = this.externalLink();
-    if (external) return external;
-    const github = this.githubLogin();
-    return github ? `https://github.com/${github}` : '';
-  });
-
-  iconName = computed<IconName>(() => (this.githubLogin() ? 'github' : 'github'));
   isRotate = input<boolean>(false);
+
+  linkText = input<string>();
+  linkUrl = input<string>();
+  icon = input<IconName>('github');
 }
