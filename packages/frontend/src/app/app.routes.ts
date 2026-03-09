4156d1170de5322f9a09fd30d25c4ac841ce8e8d
Mikita Kern <nck200069@gmail.com>
2026-03-09 18:31:47 +0100

    feat: wip

--- packages/frontend/src/app/app.routes.ts
+++ packages/frontend/src/app/app.routes.ts
@@ -1,7 +1,11 @@
 import { HomeComponent } from '@/pages/home/home.component';
 import { Routes } from '@angular/router';
+import { SignInComponent } from '@/pages/sign-in/sign-in.component';
+import { SignUpComponent } from '@/pages/sign-up/sign-up.component';
 
 export const routes: Routes = [
+  { path: 'sign-in', component: SignInComponent },
+  { path: 'sign-up', component: SignUpComponent },
   { path: '', component: HomeComponent },
   { path: '**', redirectTo: '' },
 ];
