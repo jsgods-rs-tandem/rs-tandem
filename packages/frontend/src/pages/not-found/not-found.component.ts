f7929e3cd2207f22b8cef17ad65903a37db71589
Diana Dukhovskaya <k.dianavv@gmail.com>
2026-03-16 20:34:03 +0000

    feat: implement not-forund page

--- packages/frontend/src/pages/not-found/not-found.component.ts
+++ packages/frontend/src/pages/not-found/not-found.component.ts
@@ -1,9 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, computed, inject } from '@angular/core';
+import { ButtonComponent } from '@/shared/ui';
+import { ROUTE_PATHS } from '@/core/constants';
+import { AuthService } from '@/core/services/auth.service';
 
 @Component({
   selector: 'app-not-found',
-  imports: [],
+  imports: [ButtonComponent],
   templateUrl: './not-found.component.html',
   styleUrl: './not-found.component.scss',
 })
-export class NotFoundComponent {}
+export class NotFoundComponent {
+  private authService = inject(AuthService);
+  readonly ROUTE_PATHS = ROUTE_PATHS;
+
+  readonly title = '404';
+  readonly text = "We couldn't find that page. Maybe it's playing hide and seek?";
+  readonly buttonText = computed(() =>
+    this.authService.isAuthenticated() ? 'Back to Library' : 'Go Home',
+  );
+  readonly buttonPath = computed(() =>
+    this.authService.isAuthenticated() ? ROUTE_PATHS.library : ROUTE_PATHS.home,
+  );
+}
