f616459a4a7efbefbf2fe555a25d88ca7e9c6976
mikhalenkadaniil <mikhalenka.d@gmail.com>
2026-03-29 22:22:14 +0200

    feat: implement layout of ai settings

--- packages/frontend/src/features/ai-settings/ai-settings.component.ts
+++ packages/frontend/src/features/ai-settings/ai-settings.component.ts
@@ -1,18 +1,47 @@
 import { SwitcherComponent } from '@/shared/switcher/switcher.component';
+import { InputComponent } from '@/shared/ui/input/input.component';
 import { LineBreakComponent } from '@/shared/ui/line-break/line-break.component';
+import { NgClass } from '@angular/common';
 import { Component, signal } from '@angular/core';
+import { ButtonComponent } from '@/shared/ui';
+
+const mockProviders = [
+  {
+    label: 'Open Router',
+    value: 'open-router',
+  },
+  {
+    label: 'OpenAI',
+    value: 'openai',
+  },
+  {
+    label: 'Gemini',
+    value: 'gemini',
+  },
+];
+
+const mockProviderSettings = {
+  provider: {
+    label: 'Open Router',
+    value: 'open-router',
+  },
+  model: 'gpt-4o',
+  apiKey: '21984uhrfsdfhuh8hfuhefshdfsihfiuhdfuihadf',
+};
 
 @Component({
   selector: 'app-ai-settings',
-  imports: [LineBreakComponent, SwitcherComponent],
+  imports: [LineBreakComponent, SwitcherComponent, InputComponent, NgClass, ButtonComponent],
   templateUrl: './ai-settings.component.html',
   styleUrl: './ai-settings.component.scss',
 })
 export class AiSettingsComponent {
   protected title = 'AI';
-  protected useRouter = signal(false);
+  protected providers = mockProviders;
+  protected useRemoteProvider = signal(false);
+  protected providerSettings = signal(mockProviderSettings);
 
   toggleProvider(event: boolean) {
-    this.useRouter.set(event);
+    this.useRemoteProvider.set(event);
   }
 }
