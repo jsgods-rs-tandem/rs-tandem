a26af2b41eece4301ba5fb3904a8ed7848aae841
mikhalenkadaniil <mikhalenka.d@gmail.com>
2026-03-29 17:20:34 +0200

    feat: add LineBreak component with styles and tests

---
+++ packages/frontend/src/shared/ui/line-break/line-break.component.spec.ts
@@ -0,0 +1,22 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { LineBreakComponent } from './line-break.component';
+
+describe('LineBreakComponent', () => {
+  let component: LineBreakComponent;
+  let fixture: ComponentFixture<LineBreakComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [LineBreakComponent],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(LineBreakComponent);
+    component = fixture.componentInstance;
+    await fixture.whenStable();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
