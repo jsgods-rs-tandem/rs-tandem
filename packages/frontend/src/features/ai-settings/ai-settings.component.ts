d0e947882559bfe7370ce4873e9325a30e4f48b7
mikhalenkadaniil <mikhalenka.d@gmail.com>
2026-03-29 18:45:41 +0200

    feat: implement switching of provider

--- packages/frontend/src/features/ai-settings/ai-settings.component.ts
+++ packages/frontend/src/features/ai-settings/ai-settings.component.ts
@@ -1,12 +1,18 @@
+import { SwitcherComponent } from '@/shared/switcher/switcher.component';
 import { LineBreakComponent } from '@/shared/ui/line-break/line-break.component';
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-ai-settings',
-  imports: [LineBreakComponent],
+  imports: [LineBreakComponent, SwitcherComponent],
   templateUrl: './ai-settings.component.html',
   styleUrl: './ai-settings.component.scss',
 })
 export class AiSettingsComponent {
-  title = 'AI';
+  protected title = 'AI';
+  protected useRouter = signal(false);
+
+  toggleProvider(event: boolean) {
+    this.useRouter.set(event);
+  }
 }
