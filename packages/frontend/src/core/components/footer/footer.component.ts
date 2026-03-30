8144d1f2534070a83d7c52ef875e7e6c39b9bef3
Diana Dukhovskaya <k.dianavv@gmail.com>
2026-03-30 13:12:11 +0100

    feat: localize footer component

--- packages/frontend/src/core/components/footer/footer.component.ts
+++ packages/frontend/src/core/components/footer/footer.component.ts
@@ -1,14 +1,16 @@
 import { EXTERNAL_LINKS } from '@/core/constants/external-links';
+import { TypedTranslocoPipe } from '@/shared/pipes/typed-transloco.pipe';
 import { ButtonComponent } from '@/shared/ui';
 import { Component } from '@angular/core';
+import { marker } from '@jsverse/transloco-keys-manager/marker';
 
 @Component({
   selector: 'app-footer',
-  imports: [ButtonComponent],
+  imports: [ButtonComponent, TypedTranslocoPipe],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss',
 })
 export class FooterComponent {
   readonly githubUrl = EXTERNAL_LINKS.githubRepo;
-  readonly buttonAriaLabel = 'Project Repository';
+  readonly githubAriaKey = marker('footer.ariaLabel.github');
 }
