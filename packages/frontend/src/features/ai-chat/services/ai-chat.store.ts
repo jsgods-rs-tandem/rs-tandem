97d05b06f370b6bf63f5f67c9bf8644df97d1ce7
mikhalenkadaniil <mikhalenka.d@gmail.com>
2026-04-03 21:18:37 +0200

    feat: handle popular openrouter errors

--- packages/frontend/src/features/ai-chat/services/ai-chat.store.ts
+++ packages/frontend/src/features/ai-chat/services/ai-chat.store.ts
@@ -119,6 +119,57 @@ export class AiChatStore {
           });
           break;
         }
+        case 402: {
+          this.errorMessage.set({
+            ...error,
+            message: 'Limit for this API key exceeded',
+          });
+          break;
+        }
+        case 403: {
+          this.errorMessage.set({
+            ...error,
+            message: 'You does not have access to this model',
+          });
+          break;
+        }
+        case 404: {
+          this.errorMessage.set({
+            ...error,
+            message: 'Not found model or this model does not exist',
+          });
+          break;
+        }
+        case 408: {
+          this.errorMessage.set({
+            ...error,
+            message: 'Request timeout. Try to clear chat history',
+          });
+          break;
+        }
+        case 422: {
+          this.errorMessage.set({
+            ...error,
+            message: 'Failed to process your request. Try to clear chat history',
+          });
+          break;
+        }
+
+        case 429: {
+          this.errorMessage.set({
+            ...error,
+            message: 'To many requests. Try again later',
+          });
+          break;
+        }
+
+        case 500: {
+          this.errorMessage.set({
+            ...error,
+            message: 'Internal Server Error. Try to change model or try again later',
+          });
+          break;
+        }
         default: {
           console.error(error);
           this.errorMessage.set(unknownError);
